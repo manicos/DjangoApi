ba72ae5da041932a64abd57465ab98fd1346d72b
manicos <manicos@gmail.com>
2021-03-25 15:58:53 -0600

    Add New Paths

--- app/firstapp/urls.py
+++ app/firstapp/urls.py
@@ -5,7 +5,9 @@ from . import views
 urlpatterns = [
     path('',views.vista,name='vista'),
     path('dogs',views.dogs,name='dogs'),
-    path('dogs/add',views.dogsAdd,name='dogsdd'),
-    path('dogs/delete',views.dogsDelete,name='dogsdelete'),
+    path('dog/add',views.dogsAdd,name='dogsAdd'),
+    path('dog/delete',views.dogsDelete,name='dogsdelete'),
+    path('dog/get',views.dogsGet,name='dogsGet'),
+    path('dog/get/<int:dogid>',views.dogsGetId,name='dogsGetId'),
     path('types',views.types,name='types'),
 ]
--- app/firstapp/views.py
+++ app/firstapp/views.py
@@ -69,22 +69,93 @@ def dogsDelete(request):
 
         try:
             json_object = json.loads(request.body)
+            try:
+                one_entry = Dogs.objects.get(id=json_object["dog_id"])
+            except:
+                responseData = {}
+                responseData['success'] = 'false'
+                responseData['message'] = 'The dog_id its not valid'
+                return JsonResponse(responseData, status=400)
+            Dogs.objects.filter(id=json_object["dog_id"]).delete()
             responseData = {}
             responseData['success'] = 'true'
-            responseData['data'] = 'Valid Json'
+            responseData['message'] = 'The dog has been deleted'
             return JsonResponse(responseData, status=200)
         except ValueError as e:
             responseData = {}
             responseData['success'] = 'false'
             responseData['data'] = 'Invalid Json'
             return JsonResponse(responseData, status=400)
-
     else:
 
         responseData = {}
         responseData['success'] = 'false'
         responseData['mesage'] = 'Wrong Method'
         return JsonResponse(responseData, status=400)
+
+def dogsGet(request):
+
+    if request.method == 'POST':
+
+        try:
+            json_object = json.loads(request.body)
+            try:
+                one_entry = Dogs.objects.get(id=json_object["dog_id"])
+            except:
+                responseData = {}
+                responseData['success'] = 'false'
+                responseData['message'] = 'The dog_id its not valid'
+                return JsonResponse(responseData, status=400)
+            responseData = {}
+            responseData['success'] = 'true'
+            responseData['data'] = {}
+            responseData['data']['name'] = one_entry.name
+            responseData['data']['size'] = one_entry.size
+            responseData['data']['color'] = one_entry.color
+            responseData['data']['type_id'] = one_entry.type_id
+
+            return JsonResponse(responseData, status=200)
+        except ValueError as e:
+            responseData = {}
+            responseData['success'] = 'false'
+            responseData['data'] = 'Invalid Json'
+            return JsonResponse(responseData, status=400)
+    else:
+
+        responseData = {}
+        responseData['success'] = 'false'
+        responseData['mesage'] = 'Wrong Method'
+        return JsonResponse(responseData, status=400)
+
+def dogsGetId(request, dogid):
+
+    if request.method == 'GET':
+       
+        try:
+            one_entry = Dogs.objects.get(id=dogid)
+        except:
+            responseData = {}
+            responseData['success'] = 'false'
+            responseData['message'] = 'The dog_id its not valid'
+            return JsonResponse(responseData, status=400)
+        
+        responseData = {}
+        responseData['success'] = 'true'
+        responseData['data'] = {}
+        responseData['data']['name'] = one_entry.name
+        responseData['data']['size'] = one_entry.size
+        responseData['data']['color'] = one_entry.color
+        responseData['data']['type_id'] = one_entry.type_id
+
+        return JsonResponse(responseData, status=200)
+      
+    else:
+
+        responseData = {}
+        responseData['success'] = 'false'
+        responseData['mesage'] = 'Wrong Method'
+        return JsonResponse(responseData, status=400)
+
 
 def types(request):
 
